0f6fe49dbc154e0e7affec8f8a8ed9030b1c3e63
Bill Blum <bill.blum@gmail.com>
2026-08-01 11:58:17 -0400

    Fix default unit test, 44% coverages on gcovr

--- cpp/test/cpp_catch2_unittest/GildedRoseCatch2UnitTests.cc
+++ cpp/test/cpp_catch2_unittest/GildedRoseCatch2UnitTests.cc
@@ -1,11 +1,11 @@
 #include <catch2/catch_all.hpp>
 #include "GildedRose.h"
 
-TEST_CASE("GildedRoseUnitTest", "Foo")
+TEST_CASE("Fix default unit test to verify unit tests/gcovr")
 {
     std::vector<Item> items;
-    items.push_back(Item("Foo", 0, 0));
+    items.push_back(Item("Dave's Arbitrary Item", 0, 0));
     GildedRose app(items);
     app.updateQuality();
-    REQUIRE("fixme" == app.items[0].name);
+    REQUIRE("Dave's Arbitrary Item" == app.items[0].name);
 }
