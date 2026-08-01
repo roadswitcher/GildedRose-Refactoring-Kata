472399805a2a890579de152206b02d41ebe16a1f
Bill Blum <bill.blum@gmail.com>
2026-08-01 12:02:08 -0400

    Update gitignore, add the rules from the Requirements markdown as a punchlist in the unit test file.

--- cpp/test/cpp_catch2_unittest/GildedRoseCatch2UnitTests.cc
+++ cpp/test/cpp_catch2_unittest/GildedRoseCatch2UnitTests.cc
@@ -9,3 +9,18 @@ TEST_CASE("Fix default unit test to verify unit tests/gcovr")
     app.updateQuality();
     REQUIRE("Dave's Arbitrary Item" == app.items[0].name);
 }
+
+// Pretty simple, right? Well this is where it gets interesting:
+
+// - Once the sell by date has passed, `Quality` degrades twice as fast
+// - The `Quality` of an item is never negative
+// - __"Aged Brie"__ actually increases in `Quality` the older it gets
+// - The `Quality` of an item is never more than `50`
+// - __"Sulfuras"__, being a legendary item, never has to be sold or decreases in `Quality`
+// - __"Backstage passes"__, like aged brie, increases in `Quality` as its `SellIn` value approaches;
+//     - `Quality` increases by `2` when there are `10` days or less and by `3` when there are `5` days or less but
+//     - `Quality` drops to `0` after the concert
+
+// We have recently signed a supplier of conjured items. This requires an update to our system:
+
+// - __"Conjured"__ items degrade in `Quality` twice as fast as normal items
